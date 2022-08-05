f531773f99546269a2e43f9e0296e6d53ea3bfd5
Sudha Ravi Kumar J <sudha.rkj@gmail.com>
2022-08-05 16:27:43 -0700

    Format code and support creation of data-assets and later train on each data-asset. (#4)

--- ai_services/anomaly_detection/data_preprocessing_examples/oci_data_flow_based_examples/example_code/ad_utils.py
+++ ai_services/anomaly_detection/data_preprocessing_examples/oci_data_flow_based_examples/example_code/ad_utils.py
@@ -1,18 +1,20 @@
-import datetime
 import json
-import time
 from argparse import ArgumentParser
 
-from oci.ai_anomaly_detection.models import CreateModelDetails, ModelTrainingDetails
+from oci.ai_anomaly_detection import AnomalyDetectionClient
+from oci.ai_anomaly_detection.models import CreateModelDetails, ModelTrainingDetails, CreateDataAssetDetails, \
+    DataSourceDetailsObjectStorage
+from oci.object_storage import ObjectStorageClient
 from pyspark.sql import SparkSession
 import oci
 import os
 
 DEFAULT_LOCATION = os.path.join('~', '.oci', 'config')
 DEFAULT_PROFILE = "DEFAULT"
+DEFAULT_TARGET_FAP = 0.01
+DEFAULT_TRAINING_FRACTION = 0.7
 
 
-# Helper Functions
 def get_spark_context():
     return SparkSession.builder.appName("AnomalyDetectionClient").getOrCreate()
 
@@ -44,12 +46,56 @@ def get_authenticated_client(token_path, client, file_location=DEFAULT_LOCATION,
     return authenticated_client
 
 
-def ad_train(project_id, compartment_id, data_asset_id, target_fap, training_fraction):
-    model_training_details = ModelTrainingDetails(target_fap=target_fap, training_fraction=training_fraction,
-                                                  data_asset_ids=[data_asset_id])
-    create_model_details = CreateModelDetails(compartment_id=compartment_id, project_id=project_id,
-                                              model_training_details=model_training_details)
-    return ad_client.create_model(create_model_details)
+class AdUtils:
+    def __init__(self, profile_name=DEFAULT_PROFILE, service_endpoint=None):
+        token_path = get_token_path()
+
+        client_args = {'profile_name': profile_name}
+        self.obj_client = get_authenticated_client(token_path, ObjectStorageClient, **client_args)
+
+        if service_endpoint:
+            client_args['service_endpoint'] = service_endpoint
+        self.ad_client = get_authenticated_client(token_path, AnomalyDetectionClient, **client_args)
+
+    def train(self, project_id, compartment_id, data_assets, target_fap=DEFAULT_TARGET_FAP,
+              training_fraction=DEFAULT_TRAINING_FRACTION):
+        assert data_assets['type'] == 'object_storage', "Unknown dataset details"
+        list_objects_response = self.obj_client.list_objects(namespace_name=data_assets['namespace'],
+                                                             bucket_name=data_assets['bucket'],
+                                                             prefix=data_assets['prefix'])
+        assert list_objects_response.status == 200, f'Error listing objects: {list_objects_response.text}'
+        objects_details = list_objects_response.data
+
+        model_ids = []
+        for object_details in objects_details.objects:
+            if object_details.name.endswith('.csv'):
+                data_asset_id = self._create_data_asset_(project_id=project_id, compartment_id=compartment_id,
+                                                         namespace=data_assets['namespace'],
+                                                         bucket=data_assets['bucket'],
+                                                         object_name=object_details.name)
+                model_id = self._create_model_(project_id=project_id, compartment_id=compartment_id,
+                                               data_asset_id=data_asset_id, target_fap=target_fap,
+                                               training_fraction=training_fraction)
+                model_ids.append(model_id)
+        return model_ids
+
+    def _create_data_asset_(self, project_id, compartment_id, namespace, bucket, object_name):
+        data_source_details = DataSourceDetailsObjectStorage(namespace=namespace, bucket_name=bucket,
+                                                             object_name=object_name)
+        create_data_asset_details = CreateDataAssetDetails(compartment_id=compartment_id, project_id=project_id,
+                                                           data_source_details=data_source_details)
+        data_asset_create_response = self.ad_client.create_data_asset(create_data_asset_details)
+        assert data_asset_create_response.status == 200, f"Error creating data-asset: {data_asset_create_response.text}"
+        return data_asset_create_response.data.id
+
+    def _create_model_(self, project_id, compartment_id, data_asset_id, target_fap, training_fraction):
+        model_training_details = ModelTrainingDetails(target_fap=target_fap, training_fraction=training_fraction,
+                                                      data_asset_ids=[data_asset_id])
+        create_model_details = CreateModelDetails(compartment_id=compartment_id, project_id=project_id,
+                                                  model_training_details=model_training_details)
+        create_model_response = self.ad_client.create_model(create_model_details)
+        assert create_model_response.status == 201, f"Error creating model: {create_model_response.text}"
+        return create_model_response.data.id
 
 
 if __name__ == '__main__':
@@ -58,31 +104,13 @@ if __name__ == '__main__':
     parser.add_argument("--service_endpoint", required=False, type=str, default=None)
     parser.add_argument("--project_id", required=True, type=str)
     parser.add_argument("--compartment_id", required=True, type=str)
-    parser.add_argument("--data_asset_id", required=True, type=str)
-    parser.add_argument("--target_fap", required=False, type=lambda v: float(v), default=0.01)
-    parser.add_argument("--training_fraction", required=False, type=lambda v: float(v), default=0.7)
-    parser.add_argument("--namespace", required=True, type=str)
-    parser.add_argument("--bucket", required=True, type=str)
-    parser.add_argument("--output", required=False, type=str,
-                        default=str(time.mktime(datetime.datetime.now().timetuple())))
+    parser.add_argument("--target_fap", required=False, type=lambda v: float(v), default=DEFAULT_TARGET_FAP)
+    parser.add_argument("--training_fraction", required=False, type=lambda v: float(v),
+                        default=DEFAULT_TRAINING_FRACTION)
+    parser.add_argument("--staging", required=True, type=str)
     args = parser.parse_args()
 
-    _token_path = get_token_path()
-
-    obj_client = get_authenticated_client(_token_path, oci.object_storage.ObjectStorageClient,
-                                          profile_name=args.profile_name)
-
-    if args.service_endpoint:
-        ad_client = get_authenticated_client(_token_path, oci.ai_anomaly_detection.AnomalyDetectionClient,
-                                             profile_name=args.profile_name, service_endpoint=args.service_endpoint)
-    else:
-        ad_client = get_authenticated_client(_token_path, oci.ai_anomaly_detection.AnomalyDetectionClient,
-                                             profile_name=args.profile_name)
-    training_response = ad_train(args.project_id, args.compartment_id, args.data_asset_id, args.target_fap,
-                                 args.training_fraction)
-    response = {
-        'status': training_response.status,
-        'headers': dict(training_response.headers),
-        'data': json.loads(str(training_response.data))
-    }
-    obj_client.put_object(args.namespace, args.bucket, args.output, json.dumps(response, indent=2, sort_keys=True))
+    ad_utils = AdUtils(profile_name=args.profile_name, service_endpoint=args.service_endpoint)
+    staging = json.loads(str(args.staging))
+    models = ad_utils.train(project_id=args.project_id, compartment_id=args.compartment_id, data_assets=staging)
+    print(f"Model ids: {models}")
